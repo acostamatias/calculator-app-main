6bdf020fe1e95c2608b80b413663cddb0ce848c4
acostamatias <69093336+acostamatias@users.noreply.github.com>
2021-05-27 19:22:19 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const themes = {
+  1: './css/theme-1.css',
+  2: './css/theme-2.css',
+  3: './css/theme-3.css'
+}
+
+const changeTheme = () => {
+  let slider = document.querySelector('#theme').value
+  let css = document.querySelector('#themecss').href = themes[slider]
+}
+
+document.querySelector('#result-number').addEventListener('click', () => {
+  let valueInput = document.querySelector('#result-number').value
+  if (valueInput == 0) {
+    document.querySelector('#result-number').value = ''
+  }
+})
+
+/* RESET */
+document.querySelector('#reset').addEventListener('click', function() {
+  document.querySelector('#result-number').value = ''
+})
+
+/* DEL */
+document.querySelector('#del').addEventListener('click', () => {
+  let del = document.querySelector('#result-number').value.toString().slice(0, -1)
+  document.querySelector('#result-number').value = del
+})
+
+const numbers = (num) => {
+  let textarea = document.querySelector('#result-number')
+  textarea.value += num.toString()
+}
+
+document.querySelector('#result').addEventListener('click', () => {
+  let add = document.querySelector('#result-number').value
+  let sign = ''
+  let position = ''
+  for (let i = 0; i < add.length; i++) {
+    if (isNaN(add.toString().charAt(i))) {
+      position = i
+      sign = add.toString().charAt(i)
+    }
+  }
+
+  let a = parseFloat(add.substring(0, position))
+  position++
+  let b = parseFloat(add.substring(position, add.length))
+
+  switch (sign) {
+    case '+':
+      document.querySelector('#result-number').value = parseFloat(a+b)
+      break;
+    case '-':
+      document.querySelector('#result-number').value = parseFloat(a-b)
+      break;
+    case '/':
+      document.querySelector('#result-number').value = parseFloat(a/b)
+      break;
+    case 'x':
+      document.querySelector('#result-number').value = parseFloat(a*b)
+      break;
+  }
+
+})
